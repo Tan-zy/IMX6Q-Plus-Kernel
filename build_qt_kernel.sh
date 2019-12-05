e178deb98c6907636f8bd157bab29a1dc13dcfa3
Tan-zy <709092209@qq.com>
2019-12-05 16:44:40 +0800

    modified kernel logo

--- build_qt_kernel.sh
+++ build_qt_kernel.sh
@@ -4,8 +4,12 @@ export CROSS_COMPILE=arm-none-linux-gnueabi-
 make imx_v7_linux_defconfig
 #make imx_v7_android_defconfig
 
-make uImage LOADADDR=0x10008000 KCFLAGS=-mno-android -j2
+make uImage LOADADDR=0x10008000 KCFLAGS=-mno-android
 
-make modules LOADADDR=0x10008000 KCFLAGS=-mno-android -j2
+make modules LOADADDR=0x10008000 KCFLAGS=-mno-android
 
-make dtbs LOADADDR=0x10008000 KCFLAGS=-mno-android -j2
+make dtbs LOADADDR=0x10008000 KCFLAGS=-mno-android
+
+cp arch/arm/boot/zImage /mnt/hgfs/share/
+cp arch/arm/boot/dts/imx6qp-topeet.dtb /mnt/hgfs/share/
+
